8b18cf3e2d9d75002baff8751a0311bfac7adba4
bstafford2017 <b.stafford2017@gmail.com>
2021-09-07 18:58:09 -0500

    More CORS config

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,8 @@ const response = (body) => ({
   statusCode: 200,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*'
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true
   },
   body: JSON.stringify(body)
 })
